52301b23ee50f5839f02c3dc9890d94182660355
ishuloges <36620001+ishuloges@users.noreply.github.com>
2019-11-17 23:18:24 +0530

    Footer
    Edit Footer

--- onstep/src/components/Homepage/Footer.js
+++ onstep/src/components/Homepage/Footer.js
@@ -6,38 +6,44 @@ class Footer extends Component {
         return (
             <div>
                 <div>
-                <div className="footer"> 
-                   <div className="container">
-                       <div className="row"> 
-                            <div className="col-lg-5 col-xs-12 about-company">
-                                <h3>onStep</h3>
-                                <p className="pr-5 text-white-50"> onStep
-                                    sacaac
+                    <div className="footer">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-5 col-xs-12 about-company">
+                                    <h4><img src="/images/logo.jpg" width={30} height={30} /><u><center>OnStep</center></u></h4>
+                                    <p className="pr-5 text-white-50"> <h5><center>Grocery Online Store</center></h5></p>
+                                    <br />
+                                    <br />
 
-                                </p>
-
-                                
+                                    <p className="pr-5 text-white-50"> © 2019 OnStep. All rights reserved</p>
 
 
-                            </div>   
 
-                            <div class="col-lg-3 col-xs-12 links"> 
-                                <h4 className="mt-lg-0 mt-sm-3">Services</h4>
-                                    <ul className="m-0 p-0">
+
+                                </div>
+
+                                <div class="col-lg-3 col-xs-12 links">
+                                    <h5 className="mt-lg-0 mt-sm-3">CONTACT US</h5>
+                                    <p className="pr-5 text-white-50"> </p>
+                                </div>
+
+                                <div class="col-lg-4 col-xs-12 location">
+                                    <h5 class="mt-lg-0 mt-sm-4">NEWSLETTER</h5>
+                                    <p className="pr-5 text-white-50">Subscribe to our newsletter to receive weekly news, updates, special offers, and exclusive discounts</p>
+                                    <div className="form-newsletter">
+                                        <label>Enter Your Email</label>
+                                        <input type="email" className="form-control" />
                                         
-                                    </ul>
-                            </div>  
-                            
-                            <div class="col-lg-4 col-xs-12 location">
-                            <h4 class="mt-lg-0 mt-sm-4">Social Media</h4>
-                           
-                            </div>
-                        </div>  
-                    </div> 
-                </div>  
-            </div> 
+                                </div>
 
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            </div >
         )
     }
 }
